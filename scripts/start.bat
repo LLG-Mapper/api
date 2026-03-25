751beeb9370f49554eba79dc1a0a8c24daf3a781
Simon Pucheu <104694344+SimonPucheu@users.noreply.github.com>
2026-03-25 22:06:31 +0100

    Update start.bat

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,6 +1,7 @@
 @REM Starting script for Windows
 @REM Run from the project root directory
 call venv\Scripts\activate.bat
-echo "Virtual environment activated."
+echo Virtual environment activated.
+echo Launching API...
 python app.py
-echo "API launched."
+echo API stopped.
